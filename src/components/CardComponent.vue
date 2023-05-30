656f12b88c15bbe74eaa06fa6a81c490511a8e35
Kanto72 <kanto72@yandex.ru>
2023-05-31 00:37:23 +0300

    вынес карточку в компонент Card

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card d-flex flex-column justify-space-between align-center pa-2">
+    <v-card-title>г. {{ name }}</v-card-title>
+    <v-card-text>
+      <div v-if="temp">
+        <span>Температура {{ temp.toFixed(0) }}</span>
+      </div>
+      <div v-if="humidity">
+        <span>Влажность воздуха {{ humidity }}%</span>
+      </div>
+      <div v-if="speed">
+        <span>Скорость ветра {{ speed.toFixed(0) }}м/с</span>
+      </div>
+      <!-- проверить - почему не выводится -->
+      <div v-if="all">
+        <span>Облачность {{ all }} %</span>
+      </div>
+      <div
+        v-if="description"
+        class="d-flex justify-center flex-column align-center"
+      >
+        <span>{{ description }}</span>
+        <img :src="`https://openweathermap.org/img/wn/${icon}.png`" alt="" />
+      </div>
+    </v-card-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+
+  props: {
+    name: String,
+    temp: Number,
+    humidity: Number,
+    speed: Number,
+    all: Number,
+    description: String,
+    icon: String,
+  },
+};
+</script>
+
+<style></style>
--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -8,12 +8,12 @@
       <form action="#" @submit="axiosWeather">
         <input type="text" placeholder="Введите город" v-model="query" />
         <v-select
-            label="Температура в Градусах"
-            :items="units"
-            :item-text="'label'"
-            variant="solo-inverted"
-            name="unit"
-          ></v-select>
+          label="Температура в Градусах"
+          :items="units"
+          :item-text="'label'"
+          variant="solo-inverted"
+          name="unit"
+        ></v-select>
         <v-btn type="submit">кнопка</v-btn>
       </form>
       <v-col cols="12 d-flex flex-wrap justify-center" style="gap: 20px">
@@ -24,31 +24,16 @@
           class="card d-flex flex-column justify-space-between align-center pa-2"
           style="max-width: 280px"
         >
-          <v-card-title>г. {{ card.name }}</v-card-title>
-          <v-card-text>
-            <div v-if="card.main.temp">
-              <span>Температура {{ card?.main?.temp.toFixed(0) }}</span>
-            </div>
-            <div v-if="card.main.humidity">
-              <span>Влажность воздуха {{ card?.main?.humidity }}%</span>
-            </div>
-            <div v-if="card.wind.speed">
-              <span>Скорость ветра {{ card?.wind?.speed.toFixed(0) }}м/с</span>
-            </div>
-            <div v-if="card.clouds.all">
-              <span>Облачность {{ card?.clouds?.all }} %</span>
-            </div>
-            <div
-              v-if="card.weather[0].description"
-              class="d-flex justify-center flex-column align-center"
-            >
-              <span>{{ card?.weather[0]?.description }}</span>
-              <img
-                :src="`https://openweathermap.org/img/wn/${card?.weather[0]?.icon}.png`"
-                alt=""
-              />
-            </div>
-          </v-card-text>
+        {{card}}
+          <card-component
+            :name="card.name"
+            :temp="card?.main?.temp"
+            :humidity="card?.main?.humidity"
+            :speed="card?.wind?.speed"
+            :all="card?.clouds?.all"
+            :description="card?.weather[0]?.description"
+            :icon="card?.weather[0]?.icon"
+          />
           <v-card-actions>
             <v-btn @click="deleteCity(card.name)">Удалить</v-btn>
           </v-card-actions>
@@ -60,13 +45,16 @@
 
 <script>
 import { getWeatherByCity } from "@/services/weather";
+import CardComponent from "./CardComponent.vue";
 export default {
   name: "WeatherApp",
-
+  components: {
+    CardComponent,
+  },
   data() {
     return {
-      query: "",
       info: [],
+      query: "",
       units: [
         { value: "standart", label: "Келивин" },
         { value: "imperial", label: "Фаренгейт" },
@@ -79,14 +67,17 @@ export default {
     axiosWeather(e) {
       e.preventDefault();
       const cityString = this.info.map((item) => item.name);
-      const findCity = cityString.find((elem) => elem.toLowerCase() === this.query.toLowerCase());
+      console.log(cityString);
+      const findCity = cityString.find(
+        (elem) => elem.toLowerCase() === this.query.toLowerCase()
+      );
       const formData = new FormData(e.target);
-      const unit = formData.get('unit')
+      const unit = formData.get("unit");
 
       if (findCity) {
         return alert("Город уже показан в карточке");
       }
-      getWeatherByCity(this.query, unit )
+      getWeatherByCity(this.query, unit)
         .then((result) => {
           this.info = this.info.concat(result);
           localStorage.setItem(
